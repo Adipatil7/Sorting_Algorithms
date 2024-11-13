b7dbf35963e6c98c9d7d68f2287c47fa9233ef9f
Adipatil7 <adyapatil00707@gmail.com>
2024-11-13 13:57:52 +0530

    Bubble Sort Change 2

--- Bubble_Sort.c
+++ Bubble_Sort.c
@@ -4,9 +4,9 @@ void bubbleSort(int a[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (a[j] > a[j + 1]) {
-                int temp = a[j];
+                int t = a[j];
                 a[j] = a[j + 1];
-                a[j + 1] = temp;
+                a[j + 1] = t;
             }
         }
     }
